8ded7113759acf33030b42cad29a51dd60175a8a
Royston Lobo <roydl.net@gmail.com>
2018-02-04 02:49:57 +0530

    GUI Integration compelete

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 import {SpeechToText} from 'react-native-watson';
- import Grammar from './Grammar';
+
+import GrammarView from './GrammarView';
+import PronunciationView from './PronunciationView';
+
 
 import {
   Platform,
@@ -14,15 +17,7 @@ import {
 
 } from 'react-native';
 
-// const width = Dimensions.get('window').width;
-// const height = Dimensions.get('window').height;
-
 const StartStop = 0;
-
-// const FirstRoute = () => <View style={[ styles.container, { backgroundColor: '#ff4081', height:300, width: Dimensions.get('window').width} ]}>
- // <Text style={[this.answerStyle()]}>{this.state.answer}</Text>
-      // </View>;
-// const SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7',height:300, width: Dimensions.get('window').width} ]} />;
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -35,33 +30,28 @@ export default class TabViewExample extends Component {
 
   this.state = {
     index: 0,
-    grammerText: "random",
+	sentencesSpoken: [],
+    currentSentence: "",
     grammar: "",
+	words: [],
     routes: [
       { key: '1', title: 'Pronunciation' },
       { key: '2', title: 'Grammar' },
     ],
     sentence: 'She only paints with bold colours; she does not like pastels',
-    answer: 'Your answer will appear as you speak',
+    answer: "",
     color:'red',
     instruction: "Start Recording"
   };
  
  this.onSpeechButtonPress = this.onSpeechButtonPress.bind(this)
   this.onStopButtonPress = this.onStopButtonPress.bind(this)
-  this.FirstRoute  = this.FirstRoute.bind(this)
-  this.SecondRoute  = this.SecondRoute.bind(this)
-
 
     this.onMicRelease = this.onMicRelease.bind(this);
     this.onMicPress = this.onMicPress.bind(this);
 }
 
 
- FirstRoute = () => <Grammar grammar = {this.state.grammerText} />
-  SecondRoute = () => <View style={[ styles.container, { backgroundColor: '#673ab7',height:300, width: width} ]} >
-<Text style={this.answerStyle()}>{this.state.grammar}</Text>
-  </View>;
 
     onMicPress = () => {
         this.onSpeechButtonPress();
@@ -69,85 +59,106 @@ export default class TabViewExample extends Component {
     
     onMicRelease = () => {
         this.onStopButtonPress();
+
+		
+		
+
+
+		this.processGrammar();
     }
  
 
+	processGrammar() {
+		var ref = this;
+		console.log("Processing grammar...");
+		fetch('https://8xzzaark26.execute-api.ap-south-1.amazonaws.com/final?text=' + this.state.currentSentence, {
+					method: 'GET',
+					headers: {
+					Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+		}).then((response) => {
+			console.log("AWSREPONSE " + response._bodyText);
+			var correctedSentence = response._bodyText.substr(1, response._bodyText.length - 2 );
+			var toUpdate = ref.state.grammar + correctedSentence + ". ";
+			console.log("SENTNCFORM " + toUpdate);
+			this.setState({
+				grammar: toUpdate
+			});
+
+			var sentenceTempArray = this.state.sentencesSpoken;
+			sentenceTempArray.push(correctedSentence);
+			this.setState({sentencesSpoken: sentenceTempArray});
 
 
-onSpeechButtonPress() {
-  var ref = this;
-  let grammerText = ""
-        if(this.state.instruction=='Start Recording')
-        // will transcribe microphone audio/
-        {
-          SpeechToText.startStreaming((error, text) =>
-        {
+		}).catch((error) => {
+			console.error(error);
+			this.setState({
+				grammar: "Failed to connect to server!"
+			});
+		});
+	}
 
-            if(text){
-              console.log("Text",text)
-              result = JSON.parse(text)
-          
-            word_confidence = result[0]['alternatives'][0]['word_confidence'] 
-            if(word_confidence){
-              this.setState({answer: ''})
-            for (var i = 0; i < word_confidence.length; i++)
-            {
-              for (var j = 0; j < word_confidence[i].length; j++)
-              {
-                    console.log('word_confidence[' + i + '][' + j + '] = ' + word_confidence[i][j]);
-                    pronounciation = word_confidence[i][j]
-                    if(j==1)
-                    {
-                      pronounciation = word_confidence[i][j]*100;
-                      if(pronounciation<50){this.setState({color:'yellow'})}
-                      else{this.setState({color:'blue'})}  
-                    }
-                  else{
-                    
-                     grammerText =  grammerText + pronounciation + " ";
-                  }
-                    this.setState({answer: this.state.answer + ":" + pronounciation})
-                    console.log(this.state.answer)
-                    
-              }
-            }
 
-              fetch('https://8xzzaark26.execute-api.ap-south-1.amazonaws.com/final?text=' + grammerText, {
-              method: 'GET',
-              headers: {
-              Accept: 'application/json',
-              'Content-Type': 'application/json',
-              },
-            }).then((response) => {
-              
-              var toUpdate = ref.state.grammar + response._bodyText.substr(1,response._bodyText.length-1) + ". ";
-              this.setState({
-              grammar: toUpdate
-            });
-            }).catch((error) => {
-              console.error(error);
-            this.setState({
-          grammar: "Failed to connect to server!"
-          });
-          });
-  
-            }
+	onSpeechButtonPress() {
+		var ref = this;
+		let sentenceText = ""
+		if(this.state.instruction=='Start Recording') {
 
-            }
-            else{
-              console.log(error)
-            }
-          })
-          this.setState({instruction: 'Stop Recording'})
-          console.log("Button Pressed");
-        }
-        else{
-          SpeechToText.stopStreaming()
-          this.setState({instruction: 'Start Recording'})
-          console.log("stop")      
-        }                
+			SpeechToText.startStreaming((error, text) => {
 
-    
+				if(text) {
+					console.log("Text",text)
+					result = JSON.parse(text)
+			
+					word_confidence = result[0]['alternatives'][0]['word_confidence'];
+
+					if(word_confidence) {
+
+						var wordArray = this.state.words;
+						wordArray.push(word_confidence);
+						this.setState({words: word_confidence});
+
+						this.setState({answer: ''});
+						for (var i = 0; i < word_confidence.length; i++) {
+							for (var j = 0; j < word_confidence[i].length; j++) {
+								console.log('word_confidence[' + i + '][' + j + '] = ' + word_confidence[i][j]);
+								pronounciation = word_confidence[i][j]
+								if(j==1) {
+									pronounciation = word_confidence[i][j]*100;
+									if(pronounciation<50) {	
+										this.setState({color:'yellow'})  
+									}
+									else {
+										this.setState({color:'blue'})
+									}  
+								} else {
+									sentenceText =  sentenceText + pronounciation + " ";
+								}
+								this.setState({answer: this.state.answer + ":" + pronounciation})
+								
+
+								console.log(this.state.answer); 
+							}
+						}
+					}
+
+					console.log("SENTENCE: " + sentenceText);
+					this.setState({ currentSentence: sentenceText });
+
+
+				}
+				else {
+					console.log("Error" + error);
+				}
+			});
+			this.setState({instruction: 'Stop Recording'})
+			console.log("Button Pressed");
+		} else {
+			SpeechToText.stopStreaming()
+			this.setState({instruction: 'Start Recording'})
+			console.log("stop")      
+		}                
   };
 
   onStopButtonPress() {
@@ -167,13 +178,41 @@ onSpeechButtonPress() {
  
   _renderHeader = props => <TabBar {...props} />;
  
-  _renderScene = SceneMap({
-    '1': this.FirstRoute,
-    '2': this.SecondRoute,
-  });
+  
+  getPronunciationData = () => {
+    console.log("getPronunciationData");
+    return this.state.answer;
+  }
+  
+  getGrammarData = () => {
+    console.log("getGrammarData");
+    return this.state.grammar;
+  }
+
+  getSentencesSpoken = () => {
+	  return this.state.sentencesSpoken;
+  }
+
+   getWords = () => {
+	  return this.state.words;
+  }
+
+
+  _renderScene = ({ route }) => {
+  switch (route.key) {
+  case '1':
+    return <PronunciationView answer={this.state.answer} getPronunciationData={this.getPronunciationData} getWords={this.getWords}/>;
+  case '2':
+    return <GrammarView grammar={this.state.grammar} getGrammarData={this.getGrammarData} getSentencesSpoken={this.getSentencesSpoken}/>;
+  default:
+    return null;
+  }
+  
+}
+
 
   render() {
-    console.log("SpeechToText initialized")
+    console.log("render")
       SpeechToText.initialize("1f340809-9ea9-4eaa-9404-83042e08f853", "IwxHoB67AAJE")
 
     return (
@@ -229,7 +268,7 @@ onSpeechButtonPress() {
   
   <View style={{width:Dimensions.get('window').width, height: 30,alignItems:'flex-start',justifyContent:'center'}}>
     
-    <Text style={{fontSize:20,fontFamily: 'sans-serif-condensed',color:'#1E434C'}}>Report:{this.state.answer}Grammar:{this.state.grammar}</Text>   
+    <Text style={{fontSize:20,fontFamily: 'sans-serif-condensed',color:'#1E434C', display:'none'}}>Report:{this.state.answer}</Text>   
        
   </View>     
 
@@ -244,8 +283,7 @@ onSpeechButtonPress() {
         renderScene={this._renderScene}
         renderHeader={this._renderHeader}
         onIndexChange={this._handleIndexChange}
-
-  />
+      />
 
   
 
